1b179af1930750f2a9bdba8940347b6451c5bc1b
Saem <saemshahriar@gmail.com>
2022-09-25 02:32:16 +0600

    first commit

---
+++ gridview/lib/gridViewDemo.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class GridViewDemo extends StatefulWidget {
+  const GridViewDemo({Key? key}) : super(key: key);
+
+  @override
+  State<GridViewDemo> createState() => _GridViewDemoState();
+}
+
+class _GridViewDemoState extends State<GridViewDemo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GridView.count(
+        crossAxisCount: 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        children: List.generate(20, (index) {
+          return Container(
+            decoration: BoxDecoration(
+              color: Colors.amber,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Center(
+              child: Text(
+                '$index',
+                style: TextStyle(
+                    color: Colors.blue,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+          );
+        }),
+      ),
+    );
+  }
+}
